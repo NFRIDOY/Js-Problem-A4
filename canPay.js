f2b199093b29c3208c4425862fef988508e6308f
NF RIDOY <nfridoy@gmail.com>
2023-08-09 00:31:14 +0600

    prb 1 2 3 5 done rmaining 4

--- canPay.js
+++ canPay.js
@@ -1,16 +1,30 @@
 function canPay(changeArray, totalDue) { 
     let sum = 0;
-    if (Array.isArray(changeArray) !== false) {
+    if (Array.isArray(changeArray) === false) {
         return "Please Provide An Array as Input";
     }
     else {
-        if (changeArray[0] === 0) {
+        if (Array.isArray(changeArray) && changeArray.length === 0) {
+            // the array is defined and has no elements
             return "Please Don't Provide An Emplty Array as Input";
         }
+        // if (changeArray[0] === 0) {
+        //     return "Please Don't Provide An Emplty Array as Input";
+        // }
         else {
             for (const i of changeArray) {
-                
+                sum += i;
+            }
+            if (sum >= totalDue) {
+                return true;
+            }
+            else {
+                return false;
             }
         }
     }
 }
+
+console.log(canPay([1,5,5],10));
+console.log(canPay([],10));
+console.log(canPay("sfghsdfh",10));
---
+++ findAddress.js
@@ -0,0 +1,3 @@
+function findAddress(obj) {
+    
+}
